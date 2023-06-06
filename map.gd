edf6afb89b8e7e325d2832518f0b756ccb49aed1
Paul Cento <paul.cento@gmail.com>
2023-06-06 15:56:03 -0400

    try reading map from save json

--- map.gd
+++ map.gd
@@ -21,38 +21,22 @@ class TerrainPcx:
 	var abc: Array
 	var id: int
 	func _init(_name, _abc, _id):
-		self.name = _name
-		self.abc = _abc
-		self.id = _id
-
-
-var map = [
-	['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
-	['plains', 'plains', 'plains', 'plains', 'plains', 'plains'],
-	['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
-	['plains', 'plains', 'plains', 'plains', 'plains', 'plains'],
-	['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
-	['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
-	['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
-	['plains', 'plains', 'plains', 'plains', 'plains', 'plains'],
-	['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
-	['plains', 'plains', 'plains', 'plains', 'plains', 'plains'],
-	['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
-	['plains', 'plains', 'plains', 'plains', 'plains', 'plains'],
-]
+		self.name = _name; self.abc = _abc; self.id = _id
 
 class TerrainCorner:
-	var tiles: Array[String] # top, right, bottom, left
+	var tiles: Array # top, right, bottom, left
 	var sorted: Array
 
 	var terrains = [
 		TerrainPcx.new('dpc', ['desert', 'plains', 'coast'], 0),
-		# TerrainPcx.new('ggc', ['grass', 'coast'], 1),
-		TerrainPcx.new('dgp', ['desert', 'grass', 'plains'], 2),
-		TerrainPcx.new('pgc', ['plains', 'grass', 'coast'], 3),
+		# TerrainPcx.new('ggc', ['grassland', 'coast'], 1),
+		TerrainPcx.new('dgp', ['desert', 'grassland', 'plains'], 2),
+		TerrainPcx.new('pgc', ['plains', 'grassland', 'coast'], 3),
+		TerrainPcx.new('cso', ['coast', 'sea', 'ocean'], 4),
+		TerrainPcx.new('ooo', ['ocean'], 5),
 	]
 
-	func _init(types: Array[String]):
+	func _init(types: Array):
 		self.tiles = types
 		var map = {}
 		for t in types:
@@ -71,8 +55,11 @@ class TerrainCorner:
 				if not e.abc.has(kind):
 					return false
 			return true
-		)[0]
-		return ts.id
+		)
+		if len(ts) == 0:
+			#print(self.sorted)
+			return terrains[0].id
+		return ts[0].id
 
 	func index_to_3d(idx: int) -> Vector2:
 		return Vector2(idx % 9, idx / 9)
@@ -87,11 +74,6 @@ class TerrainCorner:
 		var index = top + (left * 3) + (right * 9) + (bottom * 27)
 		return self.index_to_3d(index)
 
-func get_tileset_file(tc: TerrainCorner) -> int:
-	if tc.has('grass') and tc.has('plains'):
-		return 3
-	return -1
-
 func fill(cell, id, atlas):
 	self.set_cell(0, cell, id, atlas)
 
@@ -100,12 +82,23 @@ func tileIndexToTileSet(index, width) -> Vector2:
 	var col = index / width
 	return Vector2(col, row)
 
-func test():
-
+func test(width, height, tiles):
+	var map = []
+	map.resize(height)
+	for y in range(0, height):
+		map[y] = []
+		for x in range(0, width):
+			map[y].resize(width)
+	
 	self.clear_layer(0)
+	for i in range(0, len(tiles)):
+		var tile = tiles[i]
+#		var x: int = tile['xCoordinate']
+#		var y: int = tile['yCoordinate']
+#		if x % 2 == 0 and y % 2 == 0:
+#			x -= 1
+#		map[y][x] = tiles[i]['baseTerrainTypeKey']
 
-	var width = 6
-	var height = len(map)
 	for y in range(0, height):
 		for x in range(0, width):
 			var pos = Vector2(x, y)
@@ -113,10 +106,17 @@ func test():
 			var right = 'coast' if x == width-1 else map[y][x+1]
 			var top = 'coast' if y == 0 else map[y-1][x]
 			var bottom = 'coast' if y == height-1 else map[y+1][x]
-			var corner = TerrainCorner.new([top, right, bottom, left])
+			var lst = [top, right, bottom, left]
+			var corner = TerrainCorner.new(lst)
 			var sheet = corner.tileset()
 			var index = corner.position()
 			fill(pos, sheet, index)
 
 func _ready():
-	test()
+	var file = FileAccess.open('res://c7-static-map-save.json', FileAccess.READ)
+	var text = file.get_as_text()
+	var json = JSON.parse_string(text)['gameData']
+	var width: int = json['map']['numTilesWide']
+	var height: int = json['map']['numTilesTall']
+	var tiles = json['map']['tiles']
+	test(width, height, tiles)
